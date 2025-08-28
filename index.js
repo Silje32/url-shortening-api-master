5fb4f9fb3c4b071c85da3ed5f2f78d4e594fbd88
Silje32 <msh070425@gmail.com>
2025-08-28 11:15:23 +0200

    Tried to add a POST request

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const displayInfo = document.querySelector(".input-background");
 const url = "https://cleanuri.com/api/v1/shorten";
 
 async function getData() {
-  let response = await fetch(url);
+  let response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
+
   let data = await response.json();
   console.log(data);
 }

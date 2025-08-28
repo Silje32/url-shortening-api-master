ff3441b43af3ddbffab1a6b3920075831b09a021
Silje32 <msh070425@gmail.com>
2025-08-28 10:46:39 +0200

    Added the correct api url

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const url = "https://api.shrtco.de/v2/shorten?url=";
+const displayInfo = document.querySelector(".input-background");
+const url = "https://cleanuri.com/api/v1/shorten";
+
 async function getData() {
   let response = await fetch(url);
   let data = await response.json();

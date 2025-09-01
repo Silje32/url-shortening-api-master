373bc6b57820b9ddbef1e8dd54e432fc78c55bb3
Silje32 <msh070425@gmail.com>
2025-09-01 11:21:10 +0200

    Fixed my shortenMagnet variable

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const displayInfo = document.querySelector(".input-background");
-const url = "https://http://mgnet.me/api/create";
+const url = "http://mgnet.me/api/create";
+const shortenMagnet =
+  "magnet:?xt=urn:btih:a38d0fe7c05a7b9a05aab1e38f4e2c9e672e65e0&dn=Ubuntu+11.04";
 
-async function getData() {
+async function getData(magnetURI) {
   let response = await fetch(url, {
     method: "POST",
     headers: {
@@ -13,4 +15,4 @@ async function getData() {
   let data = await response.json();
   console.log(data);
 }
-getData();
+getData(shortenMagnet);
